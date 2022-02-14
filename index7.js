f8ab2d150498350a69accca1666c791cf406f955
kyosuke ito <kyosuke.ito@gmail.com>
2022-02-14 14:25:48 -0800

    add updated real time and changing alert

--- index7.js
+++ index7.js
@@ -1,15 +1,56 @@
-function updateOutput(){
-    $('iframe').contents().find('html').html(`
+function updateOutput() {
+  $("iframe").contents().find("html").html(`
         <html>
         <head>
             <style type="text/css">
-                ${$('#cssPanel').val()}
+                ${$("#cssPanel").val()}
             </style>
         </head>
 
         <body>
-            ${$('#htmlPanel').val()}
+            ${$("#htmlPanel").val()}
         </body>
         </html>
-    `)
+    `);
 }
+
+function eventListener() {
+  $(".panel").on("keydown", function () {
+    updateOutput();
+  });
+  changedAlert();
+}
+
+function changedAlert() {
+  $(".panel").on("keypress", function (e) {
+    e.preventDefault;
+    if (e.which === 13) {
+      alert("Your output has been changed!");
+    }
+  });
+}
+
+function toggle() {
+  $(document).ready(function () {
+    $(".html").click(function () {
+      $("#htmlPanel").toggle();
+      $(".html").toggle();
+    });
+  });
+  $(document).ready(function () {
+    $(".css").click(function () {
+      $("#cssPanel").toggle();
+      $(".css").toggle();
+    });
+  });
+  $(document).ready(function () {
+    $(".javascript").click(function () {
+      $("#javascriptPanel").toggle();
+      $(".javascript").toggle();
+    });
+  });
+}
+
+updateOutput();
+eventListener();
+toggle();
